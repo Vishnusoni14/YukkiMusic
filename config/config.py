7501ad21971a1feadf7b988781c924fdcdbece14
Moon <101085046+MOON-CODES@users.noreply.github.com>
2024-11-07 21:42:14 +0530

    Update config.py
    Signed-off-by: Moon <101085046+MOON-CODES@users.noreply.github.com>

--- config/config.py
+++ config/config.py
@@ -88,7 +88,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamVivek/YukkiMusic",
+    "https://github.com/MOON-CODES/YukkiMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
